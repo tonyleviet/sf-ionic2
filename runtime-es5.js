7bbf2917a9ca6c4b0ffbba8c4c36b10bb47de79e
VietLe <tony.leviet@gmail.com>
2025-03-11 20:54:42 +0700

    Updates

--- runtime-es5.js
+++ runtime-es5.js
@@ -230,7 +230,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	!function() {
-/******/ 		__webpack_require__.p = "/";
+/******/ 		__webpack_require__.p = "/sf-ionic2/";
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
